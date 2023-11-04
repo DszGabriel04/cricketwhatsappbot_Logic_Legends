84956d2e7bc6d9508895d3e072bf9c0aa2b6c71d
DszGabriel04 <dsouzagab04@gmail.com>
2023-11-04 15:03:48 +0530

    trial commit 3

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Hello World");
+import config
+import json
+import requests
+import pywhatkit
+
+
+phone_num = "+919527867744"
+msg = "Test Message 2"
+
+
+#pywhatkit.sendwhatmsg(phone_num, msg, 14, 50, wait_time=8)
+#pywhatkit.sendwhatmsg(phone_num, msg)
+pywhatkit.sendwhatmsg_instantly(phone_num, msg, 10, True, 5)
+#pywhatkit.sendwhats_image(phone_num, img, msg, 15, True, 3)
---
+++ config.py
@@ -0,0 +1,1 @@
+api_key = "6e0c52ace7mshc0f0288fe8698ecp1d1a00jsn6ca789cca9cc"
